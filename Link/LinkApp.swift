8dffaec608600145f407598dedfc94965c044303
wbuttermorequident <william.buttermore@quidenthealth.com>
2023-01-12 21:27:10 -0600

    Initial Commit

---
+++ Link/LinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkApp.swift
+//  Link
+//
+//  Created by William Buttermore on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct LinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
